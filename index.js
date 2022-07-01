b1dd87d8c70120eee75a213e42e9ea0c24582ff3
ppaka <asj0216@gmail.com>
2022-07-01 21:55:28 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const http = require('http');
+const socket = require('socket.io');
+const server = http.createServer();
+const port = 11100;
+
+function createRandNum(min, max) {
+  var ntemp = Math.floor(Math.random() * (max - min + 1)) + min;
+  return ntemp;
+}
+
+function fillZero(width, str) {
+  return str.length >= width ? str : new Array(width - str.length + 1).join('0') + str;//남는 길이만큼 0으로 채움
+}
+
+var io = socket(server, {
+  pingInterval: 10000,
+  pingTimeout: 5000
+});
+
+io.use((socket, next) => {
+  if (socket.handshake.query.token === "UNITY") {
+    next();
+  } else {
+    next(new Error("Authentication error"));
+  }
+});
+
+io.on('connection', socket => {
+  console.log('connection');
+
+  setTimeout(() => {
+    socket.emit('connection', { date: new Date().getTime(), data: "Hello Unity" })
+  }, 1000);
+
+  socket.on('hello', (data) => {
+    console.log('hello', data);
+    socket.emit('hello', { date: new Date().getTime(), data: data });
+  });
+
+  socket.on('createRoom', async (data) => {
+    console.log('createRoom', data);
+    var code = data;
+    if (code === '-1') {
+      do {
+        var randnum = createRandNum(1, 999999);
+        code = fillZero(6, String(randnum));
+      }
+      while (io.sockets.adapter.rooms.has(code));
+
+      console.log('Success: createRoom[%d]', code);
+      await socket.join(code);
+      const sockets = await io.in(code).fetchSockets();
+      var clientsOnRoom = [];
+      for (const socket of sockets) {
+        clientsOnRoom.push(socket.id);
+      }
+      await socket.emit('joinRoomSuccess', { date: new Date().getTime(), code: code, users: clientsOnRoom });
+    }
+    else {
+      code = fillZero(6, code);
+      if (io.sockets.adapter.rooms.has(code)) {
+        console.log('Fail: createRoom', code, 'is already created');
+        socket.emit('roomCreateError', { date: new Date().getTime(), code: code });
+      }
+      else {
+        console.log('Success: createRoom[%d]', code);
+
+        await socket.join(code);
+        const sockets = await io.in(code).fetchSockets();
+        var clientsOnRoom = [];
+        for (const socket of sockets) {
+          clientsOnRoom.push(socket.id);
+        }
+        await socket.emit('joinRoomSuccess', { date: new Date().getTime(), code: code, users: clientsOnRoom });
+      }
+    }
+  });
+
+  socket.on('joinRoom', async (code) => {
+    if (!io.sockets.adapter.rooms.has(code)) {
+      console.log('Fail: joinRoom', code, 'there is no room match with code');
+      socket.emit('roomJoinError', { date: new Date().getTime(), code: code });
+    }
+    else {
+      await socket.join(code);
+      console.log('roomCode:', code);
+      const sockets = await io.in(code).fetchSockets();
+      var clientsOnRoom = [];
+      for (const socket of sockets) {
+        console.log(socket.id);
+        clientsOnRoom.push(socket.id);
+      }
+      socket.emit('joinRoomSuccess', { date: new Date().getTime(), code: code, users: clientsOnRoom });
+      socket.to(code).emit('roomUserList', { date: new Date().getTime(), code: code, users: clientsOnRoom })
+    }
+  });
+
+  socket.on('leaveRoom', (code) => {
+    console.log('Working: leaveRoom', socket.id, ' -> ', code);
+    socket.leave(code);
+    socket.emit('leaveRoomSuccess', {date: new Date().getTime(), code: code});
+    socket.to(code).emit("roomUserLeft", socket.id);
+  });
+
+  socket.on("disconnecting", (reason) => {
+    for (const room of socket.rooms) {
+      if (room !== socket.id) {
+        console.log('Emit: roomUserLeft', socket.id, ' -> ', room);
+        socket.to(room).emit("roomUserLeft", socket.id);
+      }
+    }
+  });
+});
+
+
+server.listen(port, () => {
+  console.log('listening on *:' + port);
+});
